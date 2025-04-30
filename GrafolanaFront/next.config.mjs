141949afd30bf92add6ea6f8e7b8c4ab2be9ba11
Couzinié <j.couzinie@gmail.com>
2025-04-30 11:17:16 +0200

    Add ESLint configuration to Next.js setup

--- GrafolanaFront/next.config.mjs
+++ GrafolanaFront/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+      // Warning: Only disable this for temporary testing
+      ignoreDuringBuilds: true,
+    },
+  };
 
 export default nextConfig;
